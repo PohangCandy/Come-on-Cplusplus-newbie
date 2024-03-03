bdfca4695259d810119e102084f5f28d06c94a59
PohangCandy <rlcks777@naver.com>
2024-03-03 17:37:11 +0900

    Day3 Function

---
+++ HelloC++.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+	int FirstMoney = 50;
+	int TargetMoney = 250;
+	int CurMoney = FirstMoney;
+	int NumWinning = 0;
+
+	while (CurMoney > 0 && CurMoney < TargetMoney)
+	{
+		if (rand() % 2 > 0)
+		{
+			CurMoney ++;
+			NumWinning++;
+		}
+		else 
+		{
+			CurMoney --;
+		}
+	}
+
+	cout << CurMoney<<endl;
+	cout << NumWinning<<endl;
+
+
+	return 0;
+}
