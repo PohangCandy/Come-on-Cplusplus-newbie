99aff955dce2784cd8ec7482e7e37e9c9bbbf6c2
PohangCandy <rlcks777@naver.com>
2024-03-04 23:51:44 +0900

    Studing Default Argument

--- HelloC++.cpp
+++ HelloC++.cpp
@@ -2,23 +2,26 @@
 
 using namespace std;
 
-void swap(int& x, int& y)
+void display(char c = 'q', int n = 10)
 {
-	int tmp = x;
-	x = y;
-	y = tmp;
+	for (int i = 0; i < n; i++)
+	{
+		cout << c;
+	}
+	cout << endl;
 }
 
 int main()
 {
-	int a = 10;
-	int b = 20;
-	cout << "a: " << a << endl;
-	cout << "b: " << b << endl;
-	swap(a, b);
-	cout << "a: " << a << endl;
-	cout << "b: " << b << endl;
 
+	cout << "아무런 인수가 전달되지 않은 경우: \n";
+	display();
+
+	cout << "\n첫 번째 인수만 전달되는 경우: \n";
+	display('#');
+
+	cout << "\n모든 인수가 전달되는 경우: \n";
+	display('#', 5);
 
 	return 0;
 }
