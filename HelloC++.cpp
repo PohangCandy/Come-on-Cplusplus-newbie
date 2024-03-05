1e871b3ed93190470b36219bf8b206902513fcc3
PohangCandy <rlcks777@naver.com>
2024-03-06 01:56:21 +0900

    Day4 OOP

--- HelloC++.cpp
+++ HelloC++.cpp
@@ -3,29 +3,25 @@
 
 using namespace std;
 
+class Circle {
+public:
+	int radius;
+	string color;
+
+	double clacArea();
+};
+
+double Circle::clacArea() {
+	return 3.14 * radius * radius;
+};
+
 
 int main()
 {
-	string First;
-	string Second;
-	int i = 0;
-	int distance = 0;
+	Circle num1;
+	num1.radius = 10;
+	num1.color = "red";
 
-	cout << "첫 번째 문자열을 입력하시오." << endl;
-	getline(cin, First);
-	cout << endl;
-	cout << "두 번째 문자열을 입력하시오." << endl;
-	getline(cin, Second);
-	cout << endl;
-	for (char word : First)
-	{
-		if (word != Second[i])
-		{
-			distance++;
-		}
-		i++;
-	}
-	cout << "해밍거리: " << distance;
+	cout << "num1의 면적은: " << num1.clacArea();
 
-	return 0;
 }
